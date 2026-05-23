a0833f6c3dccb9460f732e9f2480ab08d2427523
Snoeriza <165824736+Snoeriza@users.noreply.github.com>
2026-05-23 16:52:41 +0700

    fix : push web route

--- apps/mobile/next.config.js
+++ apps/mobile/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@aqms/shared'],
+  async rewrites() {
+    return [
+      {
+        source: '/firebase-messaging-sw.js',
+        destination: '/api/firebase-sw',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
